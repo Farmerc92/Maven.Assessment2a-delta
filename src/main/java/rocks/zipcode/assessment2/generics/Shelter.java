d4b0125c8ea69b915bfc7fb1b37d3650bd8eefef
Chris Farmer <farmerc@udel.edu>
2020-03-02 11:55:02 -0500

    157/159

--- src/main/java/rocks/zipcode/assessment2/generics/Shelter.java
+++ src/main/java/rocks/zipcode/assessment2/generics/Shelter.java
@@ -4,6 +4,7 @@ package rocks.zipcode.assessment2.generics;
 import rocks.zipcode.assessment2.generics.ageable.Ageable;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * Shelter is a generic class that holds Objects that extends `Ageable`.
@@ -12,7 +13,7 @@ import java.util.ArrayList;
  * Shelter<Person> farmHouse = new Shelter<Person>();
  * Shelter<Dog> dogHouse = new Shelter<Dog>();
  */
-public class Shelter<p> {
+public class Shelter<p> implements Iterable<p>{
 
     private ArrayList<p> shelter;
     public Shelter() {
@@ -44,5 +45,10 @@ public class Shelter<p> {
 
     public Integer getIndexOf(Object ageable) {
         return shelter.indexOf(ageable);
+    }
+
+    @Override
+    public Iterator<p> iterator() {
+        return null;
     }
 }
