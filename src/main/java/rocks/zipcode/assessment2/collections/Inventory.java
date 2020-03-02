dd0c3076354830458dfc1f064010df27f4dfbf05
Chris Farmer <farmerc@udel.edu>
2020-03-02 11:51:58 -0500

    156/159

--- src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
+++ src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
@@ -1,5 +1,6 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,33 +8,34 @@ import java.util.List;
  */
 public class Inventory {
 
-
+    private List<String> inventory;
     /**
      * @param strings list of strings to add / remove / fetch from
      */
     public Inventory(List<String> strings) {
-
+        inventory = new ArrayList<>();
+        inventory.addAll(strings);
     }
 
     /**
      * nullary constructor initializes a new list
      */
     public Inventory() {
-
+        inventory = new ArrayList<>();
     }
 
     /**
      * @param item - increment the number of this item in stock by 1
      */
     public void addItemToInventory(String item) {
-        return;
+        inventory.add(item);
     }
 
     /**
      * @param item - decrement the number of this item in stock by 1
      */
     public void removeItemFromInventory(String item) {
-        return;
+        inventory.remove(inventory.indexOf(item));
     }
 
     /**
@@ -41,6 +43,12 @@ public class Inventory {
      * @return - return the number of items
      */
     public Integer getItemQuantity(String item) {
-        return null;
+        Integer count = 0;
+        for (String s : inventory) {
+            if (s.equals(item)){
+                count++;
+            }
+        }
+        return count;
     }
 }
--- src/main/java/rocks/zipcode/assessment2/collections/MonthConversion.java
+++ src/main/java/rocks/zipcode/assessment2/collections/MonthConversion.java
@@ -1,15 +1,24 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Use a map to solve
  */
 public class MonthConversion {
+
+    Map<Integer, String> calender;
     /**
      * @param monthNumber - ordinal of month in the year; i.e. January = 1, February = 2
      * @param monthName - name of month
      */
-    public void add(Integer monthNumber, String monthName) {
 
+    public MonthConversion(){
+        calender = new HashMap<>();
+    }
+    public void add(Integer monthNumber, String monthName) {
+        calender.put(monthNumber, monthName);
     }
 
     /**
@@ -17,15 +26,20 @@ public class MonthConversion {
      * @return the name of the respective month
      */
     public String getName(Integer monthNumber) {
-        throw new NullPointerException();
+        return calender.get(monthNumber);
     }
 
     /**
      * @param monthName - name of month
      * @return - the ordinal of the month in the year
      */
-    public int getNumber(String monthName) {
-        return (Integer)null;
+    public Integer getNumber(String monthName) {
+        for (Map.Entry<Integer, String> entry : calender.entrySet()){
+            if (monthName.equals(entry.getValue())){
+                return entry.getKey();
+            }
+        }
+        return null;
     }
 
     /**
@@ -33,7 +47,7 @@ public class MonthConversion {
      * @return true if the monthNumber is in the keySet
      */
     public Boolean isValidNumber(Integer monthNumber) {
-        return null;
+        return calender.containsKey(monthNumber);
     }
 
     /**
@@ -41,14 +55,14 @@ public class MonthConversion {
      * @return true if the monthName is in the valueSet
      */
     public Boolean isValidMonth(String monthName) {
-        return null;
+        return calender.containsValue(monthName);
     }
 
     /**
      * @return number of entries in this mapping
      */
     public Integer size() {
-        return -1;
+        return calender.size();
     }
 
     /**
@@ -56,6 +70,6 @@ public class MonthConversion {
      * @param monthName - name of month
      */
     public void update(Integer monthNumber, String monthName) {
-
+        calender.replace(monthNumber, monthName);
     }
 }
